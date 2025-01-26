a2cd9216482d02bd3b00ee333c6ddd40ca60e82a
ahguo2018 <6177079@qq.com>
2025-01-26 23:40:20 +0800

    first commit

---
+++ src/main/java/com/gcc/demo/GccDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gcc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GccDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GccDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gcc/demo/GccDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GccDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
